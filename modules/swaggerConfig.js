3d3ffd6170627a8a6b008fd7dc44921a03fbc710
VasilenkoDenis <vasilenko397@gmail.com>
2026-07-29 10:37:21 +0300

    Додали захист від CSRF-атак

--- modules/swaggerConfig.js
+++ modules/swaggerConfig.js
@@ -5,9 +5,52 @@ const options = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'My API',
+      title: 'PRRO backend API',
       version: '1.0.0',
-      description: 'This is a sample API',
+      description: 'This is the API for the PRRO backend application. It provides endpoints for user registration, authentication, and other functionalities.',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+      },
+    ],
+    paths: {
+      '/sign-in': {
+        post: {
+          summary: 'Реєстрація користувача',
+          description: 'Реєстрація користувача в системі.',
+          tags: ['Authentication'],
+          operationId: 'signIn',
+          requestBody: {
+            required: true,
+            content: {
+              'application/json': {
+                schema: {
+                  type: 'object',
+                  properties: {
+                    email: {
+                      type: 'string', 
+                      description: 'Електронна адреса користувача',
+                      example: 'user@example.com',
+                    },
+                    password: {
+                      type: 'string',
+                      description: 'Пароль користувача',
+                      example: 'password123',
+                    },
+                  },
+                  required: ['email', 'password'],
+                },
+              },
+            },
+          },
+          responses: {
+            '200': {
+              description: 'Успішна реєстрація користувача',
+            },
+          },
+        },
+      },
     },
   },
   apis: ['./routes/*.js', './server.js'],
--- server.js
+++ server.js
@@ -9,7 +9,7 @@
 // DataCentre -185.250.23.89:10400 (Domain name: rro.ics-market.com.ua)
 //const dbConnectionString = 'mongodb://snmp_user:Service_RW@213.160.150.219:10400/snmp_main_db';
 //const KSEFServerConnectionString = 'mongodb://mainUSER:KSEFpswd_2021@213.160.150.219:10400/ksef_main_db';
-const dbConnectionString = 'mongodb://snmp_user:Service_RW@rro.ics-market.com.ua:10400/snmp_main_db';
+const dbConnectionString = 'mongodb://prroBackendUSER:PRROBackendPswd_2026@localhost:27017/prro_backend_db';
 const KSEFServerConnectionString = 'mongodb://mainUSER:KSEFpswd_2021@rro.ics-market.com.ua:10401/ksef_main_db';
 const FirmwareDBConnectionString = 'mongodb://userDB:fWarePSWD_2025@rro.ics-market.com.ua:10400/firmware_db';
 const helmet = require('helmet'); // Helmet допомагає захистити ваші Express-додатки, встановлюючи різні HTTP-заголовки. Це не чарівна куля, але може допомогти!
@@ -30,6 +30,7 @@ const flash = require('express-flash'); // express-flash - це middleware дл�
 const swaggerConfig = require('./modules/swaggerConfig'); // Модуль swaggerConfig.js надає конфігурацію для інтеграції Swagger UI у ваш додаток Express. Він використовує бібліотеки swagger-jsdoc та swagger-ui-express для генерації документації API на основі JSDoc-коментарів у вашому коді та відображення її у вигляді інтерактивного інтерфейсу користувача.
 var Tokens = require('csrf'); // Модуль csrf надає захист від атак CSRF (Cross-Site Request Forgery) у веб-додатках. Він генерує унікальні токени для кожного користувача та перевіряє їх при обробці запитів, щоб переконатися, що запит походить від авторизованого користувача.
 var tokens = new Tokens();
+const csrf = require('csurf'); // Модуль csurf - це middleware для Express, який забезпечує захист від атак CSRF (Cross-Site Request Forgery). Він генерує унікальні токени для кожного користувача та перевіряє їх при обробці запитів, щоб переконатися, що запит походить від авторизованого користувача.
 var passport = require('passport'); // Passport - це middleware для Node.js, який забезпечує аутентифікацію користувачів у веб-додатках. Він підтримує різні стратегії аутентифікації, такі як локальна аутентифікація, OAuth, OpenID та інші, що дозволяє легко інтегрувати аутентифікацію у ваш додаток.
 var utils = require('./modules/utils'); // Модуль utils.js надає допоміжні функції для генерації токенів та випадкових чисел. Він містить функцію generateToken, яка генерує випадковий рядок заданої довжини, використовуючи символи латинського алфавіту та цифри. Функція getRandomInt використовується для отримання випадкового цілого числа у заданому діапазоні.
 var secret = tokens.secretSync();
@@ -1037,7 +1038,18 @@ app.use(session({
     cookie: { maxAge: 30 * 60 * 1000 }  // 30 min
   }));
 app.use(flash());
-//app.use(csrf({cookie:true})); 
+//app.use(csrf({cookie:true}));
+
+// Настройка csurf с хранением токена в cookie
+const csrfProtection = csrf({
+  cookie: {
+    httpOnly: true, // токен нельзя прочитать из JS
+    secure: false,  // true для HTTPS
+    sameSite: 'strict' // защита от CSRF
+  }
+});
+
+app.use(csrfProtection);
 
 passport.serializeUser(function(user, done) {
     done(null, user);
@@ -1168,7 +1180,7 @@ passport.deserializeUser(function(obj, done) {
 app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(passport.authenticate('remember-me',{failureRedirect:'/sign-in'}));
+//app.use(passport.authenticate('remember-me',{failureRedirect:'/sign-in'}));
     
 // // error handler
 // app.use((err, req, res, next)=> {
@@ -1221,6 +1233,40 @@ else
     return res.redirect('/sign-in');
 });
     
+
+
+// Эндпоинт для получения CSRF-токена
+/**
+ * @openapi
+ * /csrf-token:
+ *  get:
+ *    summary: Получение CSRF-токена
+ *   description: Возвращает CSRF-токен для защиты от CSRF-атак.
+ *   tags:
+ *     - Security
+ *  responses:
+ *    200:
+ *     description: Успешно возвращен CSRF-токен.
+ *  
+ *  
+ */
+app.get('/csrf-token', (req, res) => {
+  res.json({ csrfToken: req.csrfToken() });
+});
+
+
+/**
+ * @openapi
+ * /sign-in:
+ *   get:
+ *     summary: Отображение страницы входа
+ *     description: Возвращает страницу авторизации пользователя.
+ *     tags:
+ *       - Authentication
+ *     responses:
+ *       200:
+ *         description: Страница входа успешно отдана.
+ */
 app.get('/sign-in',function(req,res){
     return res.render('sign-in',{page:'Home', menuId:'home'});
 });
@@ -2136,6 +2182,19 @@ app.get('/rro_flasher', function(req,res){
     }
 });
 
+
+/**
+ * @openapi
+ * /registration:
+ *   get:
+ *     summary: Отображение страницы регистрации
+ *     description: Возвращает страницу регистрации нового пользователя.
+ *     tags:
+ *       - Authentication
+ *     responses:
+ *       200:
+ *         description: Страница регистрации успешно отдана.
+ */
 app.get('/registration', urlencodedParser, function(req,res){
     // console.log(req.csrfToken());
     // var token = req.csrfToken();
@@ -2144,7 +2203,38 @@ app.get('/registration', urlencodedParser, function(req,res){
     return res.render('registration',{page:'Home', menuId:'home', data:{}, errors:{}, csrfToken:tokens.create(secret)});
     // res.render('registration',{page:'Home', menuId:'home', data:{}, errors:{}});
 });
-    
+   
+/**
+ * @openapi
+ * /registration:
+ *   post:
+ *     summary: Регистрация нового пользователя
+ *     description: Создает новую учетную запись пользователя.
+ *     tags:
+ *       - Authentication
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 description: Имя пользователя
+ *               email:
+ *                 type: string
+ *                 description: Электронная почта пользователя
+ *               password:
+ *                 type: string
+ *                 description: Пароль пользователя
+ *               confirm_password:
+ *                 type: string
+ *                 description: Подтверждение пароля пользователя
+ *     responses:
+ *       200:
+ *         description: Пользователь успешно зарегистрирован.
+ */
 app.post('/registration', urlencodedParser,                          
                             check('username')
                                 .not().isEmpty()
@@ -2166,7 +2256,6 @@ app.post('/registration', urlencodedParser,
                             // check('confirm_password', 'Passwords do not match.').custom((value, {req}) => (value === req.body.password))  
                             check('confirm_password', 'Паролі не співпадають.').custom((value, {req}) => (value === req.body.password))  
                                 , function (request, response) {
-
 
     //console.log(request.csrfToken());
     if(!request.body) return response.sendStatus(400);
@@ -2861,7 +2950,7 @@ var mainconnection = mongoose.createConnection(dbConnectionString,{
     // useUnifiedTopology: true, 
     // useNewUrlParser: true,
     // // ssl: false,
-    // tls: false,
+    tls: false,
     // key:  fs.readFileSync("/etc/ssl/mongodb.pem"),
     // cert: fs.readFileSync("/etc/ssl/mongodb.crt"),
     // ca:   fs.readFileSync("/etc/ssl/mongodb-ca.crt"),
